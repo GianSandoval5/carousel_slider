73b0f1e164869ee80347c985b2fac301cc023212
Gian Sandoval <giansandoval2022@gmail.com>
2022-08-10 20:38:02 -0500

    carousel slider

---
+++ lib/data/recetas_data.dart
@@ -0,0 +1,74 @@
+import 'package:carousel_app/models/model_recetas.dart';
+
+const carruselImages = <Receta>[
+  Receta(
+    id: 1,
+    name: "Receta de Pollo",
+    description:
+        "Minim est sunt incididunt elit laboris incididunt officia. Cillum aliqua veniam minim aliquip sit adipisicing ullamco cupidatat quis ea. Duis nulla ea deserunt aliqua pariatur consectetur sit exercitation sunt. Sunt culpa labore minim aliquip non magna voluptate consectetur culpa fugiat qui incididunt.",
+    image: "assets/carrusel/10.jpg",
+  ),
+  Receta(
+    id: 2,
+    name: "Receta de Carne",
+    description:
+        "Minim est sunt incididunt elit laboris incididunt officia. Cillum aliqua veniam minim aliquip sit adipisicing ullamco cupidatat quis ea. Duis nulla ea deserunt aliqua pariatur consectetur sit exercitation sunt. Sunt culpa labore minim aliquip non magna voluptate consectetur culpa fugiat qui incididunt.",
+    image: "assets/carrusel/11.jpg",
+  ),
+  Receta(
+    id: 3,
+    name: "Receta de Pescado",
+    description:
+        "Minim est sunt incididunt elit laboris incididunt officia. Cillum aliqua veniam minim aliquip sit adipisicing ullamco cupidatat quis ea. Duis nulla ea deserunt aliqua pariatur consectetur sit exercitation sunt. Sunt culpa labore minim aliquip non magna voluptate consectetur culpa fugiat qui incididunt.",
+    image: "assets/carrusel/12.jpg",
+  ),
+  Receta(
+    id: 4,
+    name: "Receta Vegetariana",
+    description:
+        "Minim est sunt incididunt elit laboris incididunt officia. Cillum aliqua veniam minim aliquip sit adipisicing ullamco cupidatat quis ea. Duis nulla ea deserunt aliqua pariatur consectetur sit exercitation sunt. Sunt culpa labore minim aliquip non magna voluptate consectetur culpa fugiat qui incididunt.",
+    image: "assets/carrusel/13.jpg",
+  ),
+  Receta(
+    id: 5,
+    name: "Receta Colombiana",
+    description:
+        "Minim est sunt incididunt elit laboris incididunt officia. Cillum aliqua veniam minim aliquip sit adipisicing ullamco cupidatat quis ea. Duis nulla ea deserunt aliqua pariatur consectetur sit exercitation sunt. Sunt culpa labore minim aliquip non magna voluptate consectetur culpa fugiat qui incididunt.",
+    image: "assets/carrusel/14.jpg",
+  ),
+  Receta(
+    id: 6,
+    name: "Receta de Peruana",
+    description:
+        "Minim est sunt incididunt elit laboris incididunt officia. Cillum aliqua veniam minim aliquip sit adipisicing ullamco cupidatat quis ea. Duis nulla ea deserunt aliqua pariatur consectetur sit exercitation sunt. Sunt culpa labore minim aliquip non magna voluptate consectetur culpa fugiat qui incididunt.",
+    image: "assets/carrusel/15.jpg",
+  ),
+  Receta(
+    id: 7,
+    name: "Receta china",
+    description:
+        "Minim est sunt incididunt elit laboris incididunt officia. Cillum aliqua veniam minim aliquip sit adipisicing ullamco cupidatat quis ea. Duis nulla ea deserunt aliqua pariatur consectetur sit exercitation sunt. Sunt culpa labore minim aliquip non magna voluptate consectetur culpa fugiat qui incididunt.",
+    image: "assets/carrusel/16.jpg",
+  ),
+  Receta(
+    id: 8,
+    name: "Receta española",
+    description:
+        "Minim est sunt incididunt elit laboris incididunt officia. Cillum aliqua veniam minim aliquip sit adipisicing ullamco cupidatat quis ea. Duis nulla ea deserunt aliqua pariatur consectetur sit exercitation sunt. Sunt culpa labore minim aliquip non magna voluptate consectetur culpa fugiat qui incididunt.",
+    image: "assets/carrusel/17.jpg",
+  ),
+  Receta(
+    id: 9,
+    name: "Receta de japonera",
+    description:
+        "Minim est sunt incididunt elit laboris incididunt officia. Cillum aliqua veniam minim aliquip sit adipisicing ullamco cupidatat quis ea. Duis nulla ea deserunt aliqua pariatur consectetur sit exercitation sunt. Sunt culpa labore minim aliquip non magna voluptate consectetur culpa fugiat qui incididunt.",
+    image: "assets/carrusel/18.jpg",
+  ),
+  Receta(
+    id: 10,
+    name: "Chifa",
+    description:
+        "Minim est sunt incididunt elit laboris incididunt officia. Cillum aliqua veniam minim aliquip sit adipisicing ullamco cupidatat quis ea. Duis nulla ea deserunt aliqua pariatur consectetur sit exercitation sunt. Sunt culpa labore minim aliquip non magna voluptate consectetur culpa fugiat qui incididunt.",
+    image: "assets/carrusel/19.jpg",
+  ),
+];
